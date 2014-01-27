b231baa95b2a9ea7e9c53bb184a073656a7c7f52
Yahiko Uzumaki <yahiko.ninja@gmail.com>
2014-01-28 00:09:59 +0100

    Bug fix: 1st fire phase lasts 3 days instead of 2

--- battleSimulatorEU4.ts
+++ battleSimulatorEU4.ts
@@ -611,7 +611,7 @@ class Battle {
     this.defenderCur = defender;
 
     this.days = 0;
-    this.phase = "fire";
+    this.phase = "shock";
     this.dieRoll();
     this.attackerCur.setTargets(this.defenderCur);
     this.defenderCur.setTargets(this.attackerCur);
@@ -620,19 +620,16 @@ class Battle {
   nextDay() {
     this.days += 1;
 
-    if (this.days % 3 == 0) {
+    if ((this.days - 1) % 3 == 0) {
       if (this.phase == "fire") {
         this.phase = "shock";
-        this.attackerCur.setTargets(this.defenderCur);
-        this.defenderCur.setTargets(this.attackerCur);
-        this.dieRoll();
       }
       else {
         this.phase = "fire";
-        this.attackerCur.setTargets(this.defenderCur);
-        this.defenderCur.setTargets(this.attackerCur);
-        this.dieRoll();
       }
+      this.attackerCur.setTargets(this.defenderCur);
+      this.defenderCur.setTargets(this.attackerCur);
+      this.dieRoll();
     }
 
     // Calcul des dommages =============================================================
